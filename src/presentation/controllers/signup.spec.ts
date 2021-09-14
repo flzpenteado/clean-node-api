35a55a06587d966287484d7bc102a967bb307ad8
Fabio Penteado <flzpenteado@gmail.com>
2021-09-14 10:27:49 -0300

    feat: ensure SignupController returns 400 if no passwordConfirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -48,4 +48,21 @@ describe('SignupController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: password'))
   })
+
+  test('Should return 400 if no passwordConfirmation is provided', () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any@mail.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(
+      new Error('Missing param: passwordConfirmation')
+    )
+  })
 })
